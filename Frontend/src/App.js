6a5c3f08e910f12cac79e262b65c472dc00e7260
Nelani Maluka <malukanelani@gmail.com>
2024-07-09 18:05:31 +0200

    added the AuthProvider and added styling

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,5 +1,6 @@
 import Navbar from "./Components/NavBar/Navbar";
 import Home from "./Components/Home/Home";
+import AuthProvider from "./Components/Security/AuthContext";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./App.css";
@@ -7,12 +8,14 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
