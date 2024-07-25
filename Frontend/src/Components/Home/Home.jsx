c331a0458fc399b3f9d7f9e11857a86bbf8a71ba
Nelani Maluka <malukanelani@gmail.com>
2024-07-25 21:46:26 +0200

    updated styling and removed unnessesary code

--- Frontend/src/Components/Home/Home.jsx
+++ Frontend/src/Components/Home/Home.jsx
@@ -1,21 +1,11 @@
-import React, { useEffect } from "react";
 import { useAuth } from "../Security/AuthContext";
 import Section1 from "./Sections/Section1";
 import Section2 from "./Sections/Section2";
 import "./Home.css";
-import { useState } from "react";
 
 function Home() {
-  useEffect(() => {
-    AuthContext.getWeatherData();
-  }, []);
-
   const AuthContext = useAuth();
-  const [isLocationData] = useState(
-    AuthContext.isLocationData || AuthContext.getWeatherData()
-  );
-
-  console.log(isLocationData);
+  const isLocationData = AuthContext.isLocationData;
 
   return (
     <div className="home-page">
@@ -23,7 +13,6 @@ function Home() {
         <Section1 locationData={isLocationData} />
         <Section2 locationData={isLocationData} />
       </div>
-      <button>hi</button>
     </div>
   );
 }
