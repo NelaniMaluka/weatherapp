c26a40507a3d13ac1528b7008c9a94ddc5aa5130
Nelani Maluka <malukanelani@gmail.com>
2024-08-11 22:04:10 +0200

    added routes for authentication pages

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,8 +2,21 @@ import Navbar from "./Components/NavBar/Navbar";
 import Home from "./Components/Home/Home";
 import AuthProvider from "./Components/Security/AuthContext";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./Components/Dashboard/Dashboard";
+import { useAuth } from "./Components/Security/AuthContext";
+import Login from "./Components/Dashboard/Login";
+import CreateAccount from "./Components/Dashboard/CreateAccount";
+import ForgotPassword from "./Components/Dashboard/ForgotPassword";
 
 import "./App.css";
+
+function AuthenticatedRoute({ children }) {
+  const authContext = useAuth();
+  if (authContext.isAuthenticated) {
+    return children;
+  }
+  return "";
+}
 
 function App() {
   return (
@@ -13,6 +26,17 @@ function App() {
           <Navbar />
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/Login" element={<Login />} />
+            <Route path="/Create-Account" element={<CreateAccount />} />
+            <Route path="/Forgot-Password" element={<ForgotPassword />} />
+            <Route
+              path="/Dashboard"
+              element={
+                <AuthenticatedRoute>
+                  <Dashboard />
+                </AuthenticatedRoute>
+              }
+            />
           </Routes>
         </BrowserRouter>
       </AuthProvider>
