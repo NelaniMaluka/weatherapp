a34b96db4c762d9fa881a7f9ddf2aadedfc6c31a
Nelani Maluka <malukanelani@gmail.com>
2024-08-21 21:34:47 +0200

    configured corsMappings

--- Backend/src/main/java/com/WeatherApp/Backend/BackendApplication.java
+++ Backend/src/main/java/com/WeatherApp/Backend/BackendApplication.java
@@ -2,6 +2,10 @@ package com.WeatherApp.Backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -9,5 +13,15 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
-
+		
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(@NonNull CorsRegistry registry) {
+				registry.addMapping("/**")
+				.allowedMethods("*")
+				.allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 }
