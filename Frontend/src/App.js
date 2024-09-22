68e79aa7a46a674054f407e02f7bbadba8b9ad73
Nelani Maluka <malukanelani@gmail.com>
2024-09-22 21:22:34 +0200

    removed login functionality

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,21 +2,9 @@ import Navbar from "./Components/NavBar/Navbar";
 import Home from "./Components/Home/Home";
 import AuthProvider from "./Components/Security/AuthContext";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Dashboard from "./Components/Dashboard/Dashboard";
-import { useAuth } from "./Components/Security/AuthContext";
-import Login from "./Components/Dashboard/Login";
-import CreateAccount from "./Components/Dashboard/CreateAccount";
-import ForgotPassword from "./Components/Dashboard/ForgotPassword";
+import SelectedNews from "./Components/News/SelectedNews";
 
 import "./App.css";
-
-function AuthenticatedRoute({ children }) {
-  const authContext = useAuth();
-  if (authContext.isAuthenticated) {
-    return children;
-  }
-  return "";
-}
 
 function App() {
   return (
@@ -26,17 +14,7 @@ function App() {
           <Navbar />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/Login" element={<Login />} />
-            <Route path="/Create-Account" element={<CreateAccount />} />
-            <Route path="/Forgot-Password" element={<ForgotPassword />} />
-            <Route
-              path="/Dashboard"
-              element={
-                <AuthenticatedRoute>
-                  <Dashboard />
-                </AuthenticatedRoute>
-              }
-            />
+            <Route path="/News/:articleTitle" element={<SelectedNews />} />
           </Routes>
         </BrowserRouter>
       </AuthProvider>
--- Frontend/src/Components/Security/AuthContext.js
+++ Frontend/src/Components/Security/AuthContext.js
@@ -1,9 +1,6 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
 import { GetWeatherData } from "../API/Api";
 import { GetFutureWeatherData } from "../API/Api";
-import { CreateAccount } from "../API/Api";
-import { LogIn } from "../API/Api";
-import { ForgotPassword } from "../API/Api";
 import ErrorAlert from "../Alerts/ErrorAlert";
 import { GetWeatherNews } from "../API/Api";
 
@@ -15,8 +12,6 @@ function AuthProvider({ children }) {
   const [location, setLocation] = useState("Randfontein");
   const [isLocationData, setLocationData] = useState();
   const [isFutureLocationData, setFutureLocationData] = useState();
-  const [isAuthenticated, setAuthenticated] = useState(false);
-  const [isUser, setUser] = useState(null);
   const [isPopularCitiesForecast, setPopularCitiesForecast] = useState();
   const [isWeatherNews, setWeatherNews] = useState();
 
@@ -50,61 +45,8 @@ function AuthProvider({ children }) {
     try {
       const response = await GetWeatherNews();
       setWeatherNews(response.data.articles);
-      console.log(response.data.articles);
     } catch (e) {
       ErrorAlert("Couldnt get weather data");
-    }
-  }
-
-  async function login(email, password) {
-    try {
-      const response = await LogIn(email, password);
-      if (response.status === 200) {
-        setUser(response.data);
-        setAuthenticated(true);
-        return { success: true, response };
-      } else {
-        logout();
-        return { success: false, response };
-      }
-    } catch (e) {
-      logout();
-      return { success: false, response: e.response };
-    }
-  }
-
-  function logout() {
-    setAuthenticated(false);
-    setUser(null);
-  }
-
-  async function createAccount(fullName, email, password) {
-    try {
-      const response = await CreateAccount(fullName, email, password);
-      if (response.status === 200) {
-        setUser(response.data);
-        setAuthenticated(true);
-        return { success: true, response };
-      } else {
-        logout();
-        return { success: false, response };
-      }
-    } catch (e) {
-      logout();
-      return { success: false, response: e.response };
-    }
-  }
-
-  async function forgotPassword(email) {
-    try {
-      const response = await ForgotPassword(email);
-      if (response.status === 200) {
-        return { success: true };
-      } else {
-        return { success: false };
-      }
-    } catch (e) {
-      return { success: false + e.message };
     }
   }
 
@@ -139,12 +81,7 @@ function AuthProvider({ children }) {
         getFutureWeatherData,
         location,
         setLocation,
-        isAuthenticated,
-        login,
-        createAccount,
-        isUser,
         isPopularCitiesForecast,
-        forgotPassword,
         isWeatherNews,
       }}
     >
